bdf9567bdd00595622b3a6f44391df8e89c92cc0
sangavisakthiey <sangavi252000@gmail.com>
2023-07-29 11:14:13 +0530

    flappy bird

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let backgroundImage;
 let buttonX, buttonY, buttonWidth, buttonHeight;
 
 function preload() {
-    birdImg = loadImage('sprites/bluebird-downflap.png');
+    birdImg = loadImage('https://th.bing.com/th/id/OIP.QD3B67plUmuxIDkIRo4SggHaGo?w=166&h=180&c=7&r=0&o=5&pid=1.7');
     obstacleTopImg = loadImage('sprites/pipe-green.png');
     obstacleBottomImg = loadImage('sprites/pipe-red.png');
     baseImg = loadImage('sprites/base.png');
@@ -22,7 +22,7 @@ function preload() {
     gameOverSound = loadSound('audio/die.wav');
 
     // Load background image
-    backgroundImage = loadImage('images/background.jpg');
+    backgroundImage = loadImage('https://th.bing.com/th/id/OIP.9SlYwbrrXKbcoAcFd8zL6AAAAA?w=132&h=167&c=7&r=0&o=5&pid=1.7');
 }
 
 function setup() {
@@ -66,23 +66,21 @@ function draw() {
         fill(255);
         textSize(20);
         textAlign(CENTER, CENTER);
-        text("கண்ணா,நீ அவுட்டு கண்ணா..", width / 2, height / 4);
+        text("Loose your Game", width / 2, height / 4);
 
         // Draw the restart button
         fill(200);
         rect(buttonX, buttonY, buttonWidth, buttonHeight);
         fill(0);
         textSize(12);
-        text("மறுபடி விளையாடு கண்ணா.", width / 2, height / 2)
+        text("play again.", width / 2, height / 2)
     }
     fill(255);
     textSize(32);
     textAlign(CENTER, CENTER);  // Center align text
     text("Score: " + score, width / 2, 50);  // Position at center of screen
 
-    textSize(20); // Make the following texts smaller
-    text("@senthazalravi", width / 2, 75); // Place the text below the score
-    text("#pepperprogramming", width / 2, 100); // Place the text below "@senthazalravi"
+    
 
     image(baseImg, 0, height - baseImg.height * 0.75, width, baseImg.height * 0.75);
   // Draw the base image at the bottom of the screen
@@ -154,6 +152,26 @@ function Obstacle() {
     this.x = width;
     this.w = width / 8; // Adjust obstacle width based on screen width
     this.speed = 2;
+    this.show = function(obstacle) {
+        // Draw top pillar with sharp edges
+        ctx.fillStyle = 'green';
+        ctx.beginPath();
+        ctx.moveTo(this.x, 0);
+        ctx.lineTo(this.x + this.w / 2, this.top - this.w / 2);
+        ctx.lineTo(this.x + this.w, 0);
+        ctx.closePath();
+        ctx.fill();
+
+        // Draw bottom pillar with sharp edges
+        ctx.fillStyle = 'green';
+        ctx.beginPath();
+        ctx.moveTo(this.x, height - this.bottom);
+        ctx.lineTo(this.x + this.w / 2, height - this.bottom + this.w / 2);
+        ctx.lineTo(this.x + this.w, height - this.bottom);
+        ctx.closePath();
+        ctx.fill();
+      }
+
 
     this.hits = function(bird) {
         if (bird.y - birdImg.height / 2 < this.top || bird.y + birdImg.height / 2 > height - this.bottom) {
